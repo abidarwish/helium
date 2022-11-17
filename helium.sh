68f0fbab8e5b568c44409538a1e5ce07ccb78aba
Abi Darwish <42401100+abidarwish@users.noreply.github.com>
2022-11-17 11:20:23 +0800

    Add files via upload

---
+++ helium.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+#script by Abi Darwish
+
+set -e
+
+VERSIONNAME=" v"
+VERSIONNUMBER="1.0"
+GREEN='\e[1;32m'
+RED='\e[;31m'
+WHITE='\e[1m'
+NOCOLOR='\e[0m'
+
+Font_Black="\033[30m"
+Font_Red="\033[1;31m"
+Font_Green="\033[1;32m"
+Font_Yellow="\033[1;33m"
+Font_Blue="\033[1;34m"
+Font_Purple="\033[1;35m"
+Font_SkyBlue="\033[1;36m"
+Font_White="\033[1;37m"
+Font_Suffix="\033[0m"
+
+providers="/etc/dnsmasq/providers.txt"
+dnsmasqHostFinalList="/etc/dnsmasq/adblock.hosts"
+tempHostsList="/etc/dnsmasq/list.tmp"
+
+function header() {
+	echo -e $GREEN" $VERSIONNAME$VERSIONNUMBER" $NOCOLOR
+	echo -e $WHITE" by Abi Darwish" $NOCOLOR
+}
+
+function isRoot() {
+	if [ "${EUID}" != 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+	fi
+}
+
+function checkVirt() {
+	if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+	fi
+
+	if [ "$(systemd-detect-virt)" == "lxc" ]; then
+		echo "LXC is not supported (yet)."
+		exit 1
+	fi
+}
+
+function checkOS() {
+ 	if [[ $(grep -w "ID_LIKE" /etc/os-release | awk -F'=' '{print $2}') != "debian" ]]; then
+ 		echo ""
+ 		echo -e " Your OS is not supported. Please use Debian/Ubuntu"$NOCOLOR
+ 		echo ""
+ 		exit 1
+	fi
+}
+
+function initialCheck() {
+	isRoot
+	checkVirt
+	checkOS
+}
+
+function install() {
+    apt update
+    apt install dnsmasq dnsutils
+    mv /etc/dnsmasq.conf /etc/dnsmasq.conf.bak
+    wget -q -O /etc/dnsmasq.conf ""
+    wget -q -O ${providers} ""
+}
+
+function listUpdate() {
+    > ${tempHostsList}
+    while IFS= read -r line; do
+        list_url=$(echo $line | cut -d '"' -f2)
+        curl "${list_url}" 2> /dev/null | sed -E '/^!/d' | sed '/#/d' | sed -E 's/^\|\|/0.0.0.0 /g' | awk -F '^' '{print $1}' | grep -E "^0.0.0.0" | awk -F' ' '!a[$NF]++ {gsub(/^/,"0.0.0.0 ",$NF) ; print $NF ; gsub(/^(127|0)\.0\.0\.(0|1)/,"::1",$NF) ; print $NF}' | sed -E '/^0.0.0.0 0.0.0.0/d' | sed -E '/^::1 0.0.0.0/d' >> ${tempHostsList}
+    done < ${providers}
+
+    cat ${tempHostsList} | sed '/^$/d' | sort | uniq > ${dnsmasqHostFinalList}
+
+    systemctl restart dnsmasq
+    echo -e -n "Updating hostnames list..."
+    echo -e "$(cat ${dnsmasqHostFinalList} | wc -l) hostnames have been updated"
+    echo
+}
+
+initialCheck
+
+if [[ -e /etc/dnsmasq.conf ]]; then
+    if [[ ! -e /etc/dnsmasq ]]; then
+        mkdir -p /etc/dnsmasq
+    fi
+    clear
+    header
+    echo
+    listUpdate
+    echo
+    exit 0
+else
+    if [[ ! -e /etc/dnsmasq ]]; then
+        mkdir -p /etc/dnsmasq
+    fi
+    clear
+    header
+    echo
+    install
+    listUpdate
+fi
