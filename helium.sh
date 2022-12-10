1190dcf69dcacb93f414f36c796e77982a7efbc9
Abi Darwish <42401100+abidarwish@users.noreply.github.com>
2022-12-10 18:50:43 +0800

    Update helium.sh

--- helium.sh
+++ helium.sh
@@ -80,6 +80,8 @@ function install() {
 	sleep 1
 	updateEngine
 	echo "nameserver 127.0.0.1" > /etc/resolv.conf
+        echo "nameserver 127.0.0.1" > /etc/resolvconf/resolv.conf.d/head
+        > /etc/resolvconf/resolv.conf.d/original
 	#sed -i -E "/^exit 0/d" /etc/rc.local
     	#echo -e 'echo "nameserver 127.0.0.1" > /etc/resolv.conf
 #exit 0' >> /etc/rc.local
