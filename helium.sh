4a5c5b8ea2617193f68da3e0f49a81f13adc8704
Abi Darwish <42401100+abidarwish@users.noreply.github.com>
2022-12-12 18:18:42 +0800

    Update helium.sh

--- helium.sh
+++ helium.sh
@@ -324,24 +324,17 @@ function mainMenu() {
 	header
 	echo
 	echo -e " \e[1mSystem Status\e[0m"
-	
 	if [[ $(systemctl is-active dnsmasq) == active ]]; then
         	printf " %-25s %1s \e[1;32m%7s\e[0m" "Dnsmasq" ":" "running"
 		printf "\n %-25s %1s \e[1;32m%7s\e[0m" "Active since" ":" "$(systemctl status dnsmasq.service | grep -w "Active" | awk '{print $9,$10}')"
     	else
         	printf " %-25s %1s \e[1;31m%7s\e[0m" "Dnsmasq" ":" "stopped"
     	fi
-	
      	NAMESERVER=$(grep -w -E "^server" /etc/dnsmasq.conf | head -n 1 | awk -F'=' '{print $2}')
-     
      	printf "\n %-25s %1s \e[1;32m%7s\e[0m" "Nameserver" ":" "$NAMESERVER"
-	
 	printf "\n %-25s %1s \e[1;32m%'d\n\e[0m" "Blocked hostnames" ":" "$(cat /etc/dnsmasq/adblock.hosts | wc -l)"
-	
     	echo
-    
 	echo -e " \e[1mVPS Info\e[0m"
-
 	SERVER_IP=$(ip -4 addr | sed -ne 's|^.* inet \([^/]*\)/.* scope global.*$|\1|p' | awk '{print $1}' | head -1)
 	CPU=$(cat /proc/cpuinfo | grep "model\|Model" | tail -n 1 | awk -F: '{print $2}' | cut -d " " -f2-4)
 	CPU_CORE=$(lscpu | grep "CPU(s)" | head -n 1 | awk '{print $2}')
@@ -355,29 +348,19 @@ function mainMenu() {
     	UPTIME=$(uptime -p | sed 's/,//g' | awk '{print $2,$3", "$4,$5}')
 	DAILY_USAGE=$(vnstat -d --oneline | awk -F\; '{print $6}' | sed 's/ //')
 	MONTHLY_USAGE=$(vnstat -m --oneline | awk -F\; '{print $11}' | sed 's/ //')
-	
 	if [[ ${CPU_CORE} == 1 ]]; then
 	    printf " %-25s %1s %-7s\e[0m" "CPU (single core)" ":" "${CPU} @ ${CPU_MHZ}Mhz"
 	else
 	    printf " %-25s %1s %-7s\e[0m" "CPU (${CPU_CORE} cores)" ":" "${CPU} @ ${CPU_MHZ}Mhz"
 	fi
-	
 	printf "\n %-25s %1s %-7s\e[0m" "OS Version" ":" "${OS}"
-	
 	printf "\n %-25s %1s %-7s\e[0m" "Kernel Version" ":" "${KERNEL}"
-	
 	printf "\n %-25s %1s %-7s\e[0m" "RAM Usage" ":" "${RAM_USED}MB / ${TOTAL_RAM}MB (${RAM_USAGE}%)"
-	
 	printf "\n %-25s %1s %-7s\e[0m" "Date" ":" "${DATE}"
-        
     	printf "\n %-25s %1s %-7s\e[0m" "Uptime" ":" "${UPTIME}"
-
  	printf "\n %-25s %1s %-7s\e[0m" "IP Address" ":" "${SERVER_IP}"
-	
 	printf "\n %-25s %1s %-7s\e[0m" "Daily Data Usage" ":" "${DAILY_USAGE}"
-	
 	printf "\n %-25s %1s %-7s\e[0m" "Monthly Data Usage" ":" "${MONTHLY_USAGE}"
-	
 	echo
 	echo
 	echo -e $WHITE" Manage Helium"$NOCOLOR
