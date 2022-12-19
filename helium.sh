ae5b2ee3c58600e2474b8a0e8725a91f032cdc0a
Abi Darwish <khoiruddins@gmail.com>
2022-12-19 10:06:10 +0800

    Update helium.sh

--- helium.sh
+++ helium.sh
@@ -40,7 +40,7 @@ function checkVirt() {
 function checkOS() {
 	if [[ $(grep -w "ID" /etc/os-release | awk -F'=' '{print $2}') -ne "debian" ]] || [[ $(grep -w "ID" /etc/os-release | awk -F'=' '{print $2}') -ne "ubuntu" ]]; then
 		clear
-        	header
+        header
  		echo
  		echo -e ${RED}" Your OS is not supported. Please use Debian/Ubuntu"$NOCOLOR
  		echo ""
@@ -58,30 +58,30 @@ function install() {
 	echo -e " Installing Helium..."
 	[[ ! -e /etc/dnsmasq ]] && mkdir -p /etc/dnsmasq
 	[[ ! -e /etc/resolv.conf.bak ]] && cp /etc/resolv.conf /etc/resolv.conf.bak
-    	if [[ $(lsof -i :53 | grep -w -c "systemd-r") -ge "1" ]]; then
-    		systemctl disable systemd-resolved
+    if [[ $(lsof -i :53 | grep -w -c "systemd-r") -ge 1 ]]; then
+    	systemctl disable systemd-resolved
 		systemctl stop systemd-resolved
 		unlink /etc/resolv.conf
 		echo "nameserver 1.1.1.1" > /etc/resolv.conf
-    	fi
-    	apt update && apt install -y dnsmasq dnsutils vnstat resolvconf
+    fi
+    apt update && apt install -y dnsmasq dnsutils vnstat resolvconf
 	systemctl enable dnsmasq
-    	mv /etc/dnsmasq.conf /etc/dnsmasq.conf.bak
+    mv /etc/dnsmasq.conf /etc/dnsmasq.conf.bak
 	rm -rf /etc/dnsmasq.conf
-    	wget -q -O /etc/dnsmasq.conf "https://raw.githubusercontent.com/abidarwish/helium/main/dnsmasq.conf"
-    	sed -i "s/YourPublicIP/${publicIP}/" /etc/dnsmasq.conf
+    wget -q -O /etc/dnsmasq.conf "https://raw.githubusercontent.com/abidarwish/helium/main/dnsmasq.conf"
+    sed -i "s/YourPublicIP/${publicIP}/" /etc/dnsmasq.conf
 	rm -rf ${providers}
-    	wget -q -O ${providers} "https://raw.githubusercontent.com/abidarwish/helium/main/providers.txt"
+    wget -q -O ${providers} "https://raw.githubusercontent.com/abidarwish/helium/main/providers.txt"
 	sleep 1
 	updateEngine
-        > /etc/resolvconf/resolv.conf.d/original
+    > /etc/resolvconf/resolv.conf.d/original
 	echo "nameserver 127.0.0.1" > /etc/resolv.conf
-        echo "nameserver 127.0.0.1" > /etc/resolvconf/resolv.conf.d/head
+    echo "nameserver 127.0.0.1" > /etc/resolvconf/resolv.conf.d/head
 	sleep 1
-    	echo -e " Installation completed"
+    echo -e " Installation completed"
 	sleep 1
-    	echo -e " Type \e[1;32mhelium\e[0m to start"
-    	echo
+    echo -e " Type \e[1;32mhelium\e[0m to start"
+    echo
 	exit 0
 }
 
@@ -113,7 +113,7 @@ function stop() {
 		echo -e $GREEN" Helium is running"$NOCOLOR
 		echo
 		read -p " Do you want to stop Helium? [y/n]: " STOP
-		if [[ $STOP == "y" ]]; then
+		if [[ ${STOP} == "y" ]]; then
 			systemctl disable dnsmasq
 			systemctl stop dnsmasq
 			echo "nameserver 1.1.1.1" > /etc/resolv.conf
@@ -141,12 +141,8 @@ function changeDNS() {
     	echo -e " Proxy server IP address to bypass Netflix"
     	read -p " (press c to cancel): " DNS
     	oldDNS=$(grep -E -w "^server" /etc/dnsmasq.conf | cut -d '=' -f2)
-    	if [[ $DNS == "c" ]]; then
-        	mainMenu
-    	fi
-    	if [[ -z $DNS ]]; then
-        	changeDNS
-    	fi
+    	[[ ${DNS} == "c" ]] && mainMenu
+    	[[ -z ${DNS} ]] && changeDNS
     	sed -i "s/server=${oldDNS}/server=${DNS}/" /etc/dnsmasq.conf
         systemctl restart dnsmasq
     	sleep 1
@@ -162,31 +158,27 @@ function reinstall() {
 	header
 	echo
 	read -p " Do you want to reinstall Helium? [y/n]: " REINSTALL
-	if [[ ${REINSTALL} != y ]]; then
-		mainMenu
-	fi
+	[[ ${REINSTALL} != "y" ]] && mainMenu
 	echo -e " Reinstalling Helium..."
 	sleep 2
-    	if [[ ! -e /etc/dnsmasq ]]; then
-		mkdir -p /etc/dnsmasq
-	fi
-    	echo "nameserver 1.1.1.1" > /etc/resolv.conf
-    	apt update && apt install -y dnsmasq dnsutils vnstat resolvconf
+    [[ ! -e /etc/dnsmasq ]] && mkdir -p /etc/dnsmasq
+    echo "nameserver 1.1.1.1" > /etc/resolv.conf
+    apt update && apt install -y dnsmasq dnsutils vnstat resolvconf
 	systemctl enable dnsmasq
 	rm -rf /etc/dnsmasq.conf
-    	wget -q -O /etc/dnsmasq.conf "https://raw.githubusercontent.com/abidarwish/helium/main/dnsmasq.conf"
-    	sed -i "s/YourPublicIP/${publicIP}/" /etc/dnsmasq.conf
+    wget -q -O /etc/dnsmasq.conf "https://raw.githubusercontent.com/abidarwish/helium/main/dnsmasq.conf"
+    sed -i "s/YourPublicIP/${publicIP}/" /etc/dnsmasq.conf
 	rm -rf ${providers}
-    	wget -q -O ${providers} "https://raw.githubusercontent.com/abidarwish/helium/main/providers.txt"
+    wget -q -O ${providers} "https://raw.githubusercontent.com/abidarwish/helium/main/providers.txt"
 	updateEngine
-        > /etc/resolvconf/resolv.conf.d/original
+    >/etc/resolvconf/resolv.conf.d/original
 	echo "nameserver 127.0.0.1" > /etc/resolv.conf
-        echo "nameserver 127.0.0.1" > /etc/resolvconf/resolv.conf.d/head
+    echo "nameserver 127.0.0.1" > /etc/resolvconf/resolv.conf.d/head
 	sleep 1
-    	echo -e " Installation completed"
+    echo -e " Installation completed"
 	sleep 1
-    	echo -e " Type \e[1;32mhelium\e[0m to start"
-    	echo
+    echo -e " Type \e[1;32mhelium\e[0m to start"
+    echo
 	exit 0
 }
 
@@ -195,72 +187,62 @@ function uninstall() {
 	header
 	echo
 	read -p " Do you want to uninstall Helium? [y/n]: " UNINSTALL
-	if [[ $UNINSTALL == "y" ]]; then
-		systemctl stop dnsmasq
-		systemctl disable dnsmasq
-		apt remove -y dnsmasq > /dev/null 2>&1
-		rm -rf /etc/dnsmasq.d
-		rm -rf /etc/dnsmasq
-                > /etc/resolvconf/resolv.conf.d/original
-                > /etc/resolvconf/resolv.conf.d/head
-		mv /etc/resolv.conf.bak /etc/resolv.conf
-		echo -e -n " Uninstalling Helium..."
-		sleep 2
-		echo -e $GREEN"done"$NOCOLOR
-		echo
-		exit 0
-	else
-		mainMenu
-	fi
+	[[ ${UNINSTALL} != "y" ]] && mainMenu
+	systemctl stop dnsmasq
+	systemctl disable dnsmasq
+	apt remove -y dnsmasq > /dev/null 2>&1
+	rm -rf /etc/dnsmasq.d
+	rm -rf /etc/dnsmasq
+    >/etc/resolvconf/resolv.conf.d/original
+    >/etc/resolvconf/resolv.conf.d/head
+	mv /etc/resolv.conf.bak /etc/resolv.conf
+	echo -e -n " Uninstalling Helium..."
+	sleep 2
+	echo -e $GREEN"done"$NOCOLOR
+	echo
+	exit 0
 }
 
 function updateEngine() {
 	echo -e -n " Updating blocked hostnames..."
-	> ${tempHostsList}
-    	while IFS= read -r line; do
-        	list_url=$(echo $line | grep -E -v "^#" | cut -d '"' -f2)
-        	curl "${list_url}" 2> /dev/null | sed -E '/^!/d' | sed '/#/d' | sed -E 's/^\|\|/0.0.0.0 /g' | awk -F '^' '{print $1}' | grep -E "^0.0.0.0" >> ${tempHostsList}
-    	done < ${providers}
-    	if [[ ! -z $(ip a | grep -w "inet6") ]]; then
-    		grep -E "^0.0.0.0" ${tempHostsList} | sed -E 's/^0.0.0.0/::1/g' >> ${tempHostsList}
+	>${tempHostsList}
+    while IFS= read -r line; do
+        list_url=$(echo $line | grep -E -v "^#" | cut -d '"' -f2)
+        curl "${list_url}" 2> /dev/null | sed -E '/^!/d' | sed '/#/d' | sed -E 's/^\|\|/0.0.0.0 /g' | awk -F '^' '{print $1}' | grep -E "^0.0.0.0" >> ${tempHostsList}
+    done <${providers}
+    if [[ ! -z $(ip a | grep -w "inet6") ]]; then
+		grep -E "^0.0.0.0" ${tempHostsList} | sed -E 's/^0.0.0.0/::1/g' >>${tempHostsList}
 	fi
-    	cat ${tempHostsList} | sed '/^$/d' | sed -E '/^0.0.0.0 0.0.0.0/d' | sed -E '/^::1 0.0.0.0/d' | sort | uniq > ${dnsmasqHostFinalList}
-	if [[ ! -e /etc/dnsmasq/whitelist.hosts ]]; then
-		touch /etc/dnsmasq/whitelist.hosts
-	fi
+    cat ${tempHostsList} | sed '/^$/d' | sed -E '/^0.0.0.0 0.0.0.0/d' | sed -E '/^::1 0.0.0.0/d' | sort | uniq >${dnsmasqHostFinalList}
+	[[ ! -e /etc/dnsmasq/whitelist.hosts ]] && touch /etc/dnsmasq/whitelist.hosts
 	DATA=$(cat /etc/dnsmasq/whitelist.hosts)
 	for HOSTNAME in ${DATA}; do
 		sed -E -i "/${HOSTNAME}/d" /etc/dnsmasq/adblock.hosts
 	done
-    	systemctl restart dnsmasq
-    	echo -e ${GREEN}"done"${NOCOLOR}
-    	sleep 1
-    	echo -e -n $GREEN" $(cat ${dnsmasqHostFinalList} | sed '/^$/d' | wc -l) "$NOCOLOR
+    systemctl restart dnsmasq
+    echo -e ${GREEN}"done"${NOCOLOR}
+    sleep 1
+    echo -e -n ${GREEN}" $(cat ${dnsmasqHostFinalList} | sed '/^$/d' | wc -l) "${NOCOLOR}
    	echo -e "hostnames have been blocked"
 }
 
 function listUpdate() {
-    	clear
-    	header
-    	echo
-    	read -p " Do you want to update blocked hostnames? [y/n]: " UPDATE
-    	if [[ $UPDATE == "y" ]]; then
-    	       updateEngine
-    	       echo
-    	       read -p " Press Enter to continue..."
-    	       mainMenu
-    	 else
-    	       mainMenu
-    	 fi
+    clear
+    header
+    echo
+    read -p " Do you want to update blocked hostnames? [y/n]: " UPDATE
+    [[ ${UPDATE} != "y" ]] && mainMenu
+	updateEngine
+    echo
+    read -p " Press Enter to continue..."
+    mainMenu
 }
 
 function activateProvider() {
 	clear
 	header
 	echo
-	if [[ ! -e /etc/dnsmasq/providers.tmp ]]; then
-	       cp /etc/dnsmasq/providers.txt /etc/dnsmasq/providers.tmp
-	fi
+	[[ ! -e /etc/dnsmasq/providers.tmp ]] && cp /etc/dnsmasq/providers.txt /etc/dnsmasq/providers.tmp
 	printf " ${WHITE}%-26s %10s${NOCOLOR}\n" "LIST PROVIDER" "STATUS"
 	echo " --------------------------------------"
 	while IFS= read -r line; do
@@ -271,7 +253,7 @@ function activateProvider() {
 		else
 			printf " %-25s \e[1;31m%12s\e[0m\n" "${INACTIVE_PROVIDER}" "inactive"
 		fi
-	done < /etc/dnsmasq/providers.tmp
+	done </etc/dnsmasq/providers.tmp
 	echo
 	if [[ ! -z $(diff -q /etc/dnsmasq/providers.tmp /etc/dnsmasq/providers.txt) ]]; then
 		read -p " Select a provider to be activated
@@ -280,7 +262,7 @@ function activateProvider() {
 		read -p " Select a provider to be activated
  (press c to cancel): " SELECT
 	fi
-    	if [[ $SELECT == s ]]; then
+    	if [[ ${SELECT} == "s" ]]; then
 		mv /etc/dnsmasq/providers.tmp /etc/dnsmasq/providers.txt
 		echo " Applying changes..."
 		updateEngine
@@ -288,13 +270,11 @@ function activateProvider() {
 		read -p " Press Enter to continue..."
 		mainMenu
     	fi
-    	if [[ $SELECT == c ]]; then
+    	if [[ ${SELECT} == "c" ]]; then
 		rm -rf /etc/dnsmasq/providers.tmp
 		mainMenu
 	fi
-	if [[ -z $SELECT ]]; then
-		activateProvider
-	fi
+	[[ -z ${SELECT} ]] && activateProvider
 	if [[ $(grep -E -c -w "^#${SELECT}" /etc/dnsmasq/providers.tmp) != 0 ]]; then
 		sed -E -i "s/^\#${SELECT}/${SELECT}/" /etc/dnsmasq/providers.tmp
 		activateProvider
@@ -310,9 +290,7 @@ function deactivateProvider() {
 	clear
 	header
 	echo
-	if [[ ! -e /etc/dnsmasq/providers.tmp ]]; then
-	       cp /etc/dnsmasq/providers.txt /etc/dnsmasq/providers.tmp
-	fi
+	[[ ! -e /etc/dnsmasq/providers.tmp ]] && cp /etc/dnsmasq/providers.txt /etc/dnsmasq/providers.tmp
 	printf " ${WHITE}%-26s %10s${NOCOLOR}\n" "LIST PROVIDER" "STATUS"
 	echo " --------------------------------------"
 	while IFS= read -r line; do
@@ -323,38 +301,36 @@ function deactivateProvider() {
 		else
 			printf " %-25s \e[1;31m%12s\e[0m\n" "${INACTIVE_PROVIDER}" "inactive"
 		fi
-	done < /etc/dnsmasq/providers.tmp
+	done </etc/dnsmasq/providers.tmp
 	echo
 	if [[ ! -z $(diff -q /etc/dnsmasq/providers.tmp /etc/dnsmasq/providers.txt) ]]; then
 		read -p " Select a provider to be activated
  (press s to save changes or c to cancel): " SELECT
-       else
-       		read -p " Select a provider to be deactivated
+    else
+       	read -p " Select a provider to be deactivated
  (press c to cancel): " SELECT
-       fi
-       if [[ $SELECT == s ]]; then
-       		mv /etc/dnsmasq/providers.tmp /etc/dnsmasq/providers.txt
+    fi
+    if [[ ${SELECT} == "s" ]]; then
+		mv /etc/dnsmasq/providers.tmp /etc/dnsmasq/providers.txt
 		echo " Applying changes..."
 		updateEngine
 		echo
 		read -p " Press Enter to continue..."
 		mainMenu
 	fi
-	if [[ $SELECT == c ]]; then
+	if [[ ${SELECT} == "c" ]]; then
 		rm -rf /etc/dnsmasq/providers.tmp
 		mainMenu
-    	fi
-    	if [[ -z $SELECT ]]; then
-        	deactivateProvider
-    	fi
+    fi
+    [[ -z ${SELECT} ]] && deactivateProvider
 	if [[ $(grep -E -c -w "^#${SELECT}" /etc/dnsmasq/providers.tmp) == 0 ]]; then
 		sed -E -i "s/^${SELECT}/\#${SELECT}/" /etc/dnsmasq/providers.tmp
 		deactivateProvider
 	else
 		echo -e " ${SELECT} is already inactive"
 	fi
-    	echo
-    	read -p " Press Enter to continue..."
+    echo
+    read -p " Press Enter to continue..."
 	deactivateProvider
 }
 
@@ -362,50 +338,42 @@ function whitelistHost() {
 	clear
 	header
 	echo
-	if [[ ! -e /etc/dnsmasq/whitelist.hosts ]]; then
-		touch /etc/dnsmasq/whitelist.hosts
-	fi
-	if [[ ! -e /etc/dnsmasq/whitelist.hosts.tmp ]]; then
-	       cp /etc/dnsmasq/whitelist.hosts /etc/dnsmasq/whitelist.hosts.tmp
-	fi
+	[[ ! -e /etc/dnsmasq/whitelist.hosts ]] && touch /etc/dnsmasq/whitelist.hosts
+	[[ ! -e /etc/dnsmasq/whitelist.hosts.tmp ]] && cp /etc/dnsmasq/whitelist.hosts /etc/dnsmasq/whitelist.hosts.tmp
 	printf " ${WHITE}%-26s %10s${NOCOLOR}\n" "HOST" "STATUS"
 	echo " --------------------------------------"
-	if [[ -z $(cat /etc/dnsmasq/whitelist.hosts.tmp) ]]; then
-		echo -e " List is empty"
-	fi
+	[[ -z $(cat /etc/dnsmasq/whitelist.hosts.tmp) ]] && echo -e " List is empty"
 	while IFS= read -r line; do
 		ACTIVE_HOST=$(echo $line)
 		printf " %-25s \e[1;32m%12s\e[0m\n" "${ACTIVE_HOST}" "whitelisted"
-	done < /etc/dnsmasq/whitelist.hosts.tmp
+	done </etc/dnsmasq/whitelist.hosts.tmp
 	echo
 	if [[ ! -z $(diff -q /etc/dnsmasq/whitelist.hosts.tmp /etc/dnsmasq/whitelist.hosts) ]]; then
 		read -p " Select a url from above to delete or type a new one to whitelist
  (press s to save changes or c to cancel): " SELECT
-        else
-       		read -p " Select a url from above to delete or type a new one to whitelist
+    else
+       	read -p " Select a url from above to delete or type a new one to whitelist
  (press c to cancel): " SELECT
-        fi
-	if [[ $SELECT == s ]]; then
-       	        mv /etc/dnsmasq/whitelist.hosts.tmp /etc/dnsmasq/whitelist.hosts
+    fi
+	if [[ ${SELECT} == "s" ]]; then
+       	mv /etc/dnsmasq/whitelist.hosts.tmp /etc/dnsmasq/whitelist.hosts
 		updateEngine
 		echo
 		read -p " Press Enter to continue..."
 		mainMenu
 	fi
-	if [[ $SELECT == c ]]; then
+	if [[ ${SELECT} == "c" ]]; then
 		rm -rf /etc/dnsmasq/whitelist.hosts.tmp
 		mainMenu
-        fi
-        if [[ -z $SELECT ]]; then
-                whitelistHost
-        fi
+    fi
+    [[ -z $SELECT ]] && whitelistHost
 	if [[ $(grep -c -w "${SELECT}" /etc/dnsmasq/whitelist.hosts.tmp) == 0 ]]; then
-		echo "${SELECT}" >> /etc/dnsmasq/whitelist.hosts.tmp
+		echo "${SELECT}" >>/etc/dnsmasq/whitelist.hosts.tmp
 		sed -i '/^$/d' /etc/dnsmasq/whitelist.hosts.tmp | sort | uniq
 		whitelistHost
 	else
 		read -p " Do you want to delete this url? [y/n]: " DELETE
-		if [[ ${DELETE} == y ]]; then
+		if [[ ${DELETE} == "y" ]]; then
 			sed -E -i "/^${SELECT}/d" /etc/dnsmasq/whitelist.hosts.tmp
 			whitelistHost
 		else
@@ -419,9 +387,7 @@ function cleaner() {
 	header
 	echo
 	read -p " Do you want to cleanup the database? [y/n]: " CLEANUP
-	if [[ ${CLEANUP} != y ]]; then
-		mainMenu
-	fi
+	[[ ${CLEANUP} != "y" ]] && mainMenu
 	ORIGINAL_DATABASE=$(cat /etc/dnsmasq/adblock.hosts | sed '/^$/d' | wc -l)
 	echo -e -n " Checking database..."
 	sleep 2
@@ -436,9 +402,7 @@ function cleaner() {
 	NEW_DATABASE=$(cat /etc/dnsmasq/adblock.hosts | sed '/^$/d' | wc -l)
 	DELETED_HOSTNAMES=$(( ORIGINAL_DATABASE-NEW_DATABASE ))
 	printf "\n \e[1;32m%'d\e[0m %-10s\n" "${DELETED_HOSTNAMES}" "dead hostnames have been deleted from the database"
-	if [[ ${DELETED_HOSTNAMES} -gt 0 ]]; then
-		systemctl restart dnsmasq
-	fi
+	[[ ${DELETED_HOSTNAMES} -gt 0 ]] && systemctl restart dnsmasq
 	echo
 	read -p " Press Enter to continue..."
 	mainMenu
@@ -469,13 +433,13 @@ function updateHelium() {
 	echo
 	echo
 	read -p " Do you want to update? [y/n]: " UPDATE
-	if [[ ${UPDATE} != y ]]; then
+	if [[ ${UPDATE} != "y" ]]; then
 		rm -rf /tmp/helium.tmp
 		mainMenu
 	fi
 	read -p " Do you want to overwrite the existing providers? [y/n]: " OVERWRITE
 	echo -n -e " Updating Helium..."
-	if [[ ${OVERWRITE} == y ]]; then
+	if [[ ${OVERWRITE} == "y" ]]; then
 		rm -rf /etc/dnsmasq/providers.txt
 		wget -q -O /etc/dnsmasq/providers.txt "https://raw.githubusercontent.com/abidarwish/helium/main/providers.txt"
 	fi
@@ -502,15 +466,15 @@ function mainMenu() {
 	echo
 	echo -e " \e[1mSystem Status\e[0m"
 	if [[ $(systemctl is-active dnsmasq) == active ]]; then
-        	printf " %-25s %1s \e[1;32m%7s\e[0m" "Dnsmasq" ":" "running"
+        printf " %-25s %1s \e[1;32m%7s\e[0m" "Dnsmasq" ":" "running"
 		printf "\n %-25s %1s \e[1;32m%7s\e[0m" "Active since" ":" "$(systemctl status dnsmasq.service | grep -w "Active" | awk '{print $9,$10,$11,$12}')"
-    	else
-        	printf " %-25s %1s \e[1;31m%7s\e[0m" "Dnsmasq" ":" "stopped"
-    	fi
-     	NAMESERVER=$(grep -w -E "^server" /etc/dnsmasq.conf | head -n 1 | awk -F'=' '{print $2}')
-     	printf "\n %-25s %1s \e[1;32m%7s\e[0m" "Nameserver" ":" "$NAMESERVER"
+    else
+        printf " %-25s %1s \e[1;31m%7s\e[0m" "Dnsmasq" ":" "stopped"
+    fi
+    NAMESERVER=$(grep -w -E "^server" /etc/dnsmasq.conf | head -n 1 | awk -F'=' '{print $2}')
+    printf "\n %-25s %1s \e[1;32m%7s\e[0m" "Nameserver" ":" "$NAMESERVER"
 	printf "\n %-25s %1s \e[1;32m%'d\n\e[0m" "Blocked hostnames" ":" "$(cat ${dnsmasqHostFinalList} | wc -l)"
-    	echo
+    echo
 	echo -e " \e[1mMachine Info\e[0m"
 	CPU=$(cat /proc/cpuinfo | grep "model\|Model" | tail -n 1 | awk -F: '{print $2}' | cut -d " " -f2-4)
 	CPU_CORE=$(lscpu | grep "CPU(s)" | head -n 1 | awk '{print $2}')
@@ -521,7 +485,7 @@ function mainMenu() {
 	TOTAL_RAM=$(free -m | grep Mem: | awk '{print $2}')
 	RAM_USAGE=$(echo "scale=2; ($RAM_USED / $TOTAL_RAM) * 100" | bc | cut -d. -f1)
 	DATE=$(date | awk '{print $2,$3,$4,$5,$6}')
-    	UPTIME=$(uptime -p | sed 's/,//g' | awk '{print $2,$3", "$4,$5}')
+    UPTIME=$(uptime -p | sed 's/,//g' | awk '{print $2,$3", "$4,$5}')
 	DAILY_USAGE=$(vnstat -d --oneline | awk -F\; '{print $6}' | sed 's/ //')
 	MONTHLY_USAGE=$(vnstat -m --oneline | awk -F\; '{print $11}' | sed 's/ //')
 	if [[ ${CPU_CORE} == 1 ]]; then
@@ -533,7 +497,7 @@ function mainMenu() {
 	printf "\n %-25s %1s %-7s\e[0m" "Kernel Version" ":" "${KERNEL}"
 	printf "\n %-25s %1s %-7s\e[0m" "RAM Usage" ":" "${RAM_USED}MB / ${TOTAL_RAM}MB (${RAM_USAGE}%)"
 	printf "\n %-25s %1s %-7s\e[0m" "Date" ":" "${DATE}"
-    	printf "\n %-25s %1s %-7s\e[0m" "Uptime" ":" "${UPTIME}"
+    printf "\n %-25s %1s %-7s\e[0m" "Uptime" ":" "${UPTIME}"
  	printf "\n %-25s %1s %-7s\e[0m" "IP Address" ":" "${publicIP}"
 	printf "\n %-25s %1s %-7s\e[0m" "Daily Data Usage" ":" "${DAILY_USAGE}"
 	printf "\n %-25s %1s %-7s\e[0m" "Monthly Data Usage" ":" "${MONTHLY_USAGE}"
